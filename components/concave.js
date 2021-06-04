47c46804d5631c8ecd4336edd707be0630634e3c
Paras Verma <paras.19508@sscbsdu.ac.in>
2021-06-04 16:55:10 +0530

    impl: dark mode, data updation

--- components/concave.js
+++ components/concave.js
@@ -4,13 +4,15 @@ class MyElement extends LitElement {
 
     static get properties() {
         return{
-            category: {type: String},
-            title: {type: String},
+            // variables for data related to article
+            category: {type: String, attribute: false},   
+            // business/technology/health/etc
+            title: {type: String},      
             content: {type: String},
             author: {type: String},
             publisher: {type: String},
-            url: {type: URL},
-            cover: {type: String},
+            url: {type: URL},           // url to the article
+            cover: {type: String},       // cover image for the article
         };
     }
 
@@ -22,7 +24,23 @@ class MyElement extends LitElement {
     }
 
     static get styles() {
+        // external css variables
+        // --bg_lvl1
+        // --bg_lvl2
+        // --bg_lvl3
+        // --shadow
+        // --text_lvl2
+        // --text_lvl1
         return css ` 
+            * {
+                transition: .4s;
+            }
+            :host {
+                border-radius: 10px;
+                overflow: hidden;
+                background-color: var(--bg_lvl2);
+                box-shadow: 10px 10px 15px var(--shadow);
+            }
             a {
                 text-decoration: none;
             }       
@@ -33,9 +51,6 @@ class MyElement extends LitElement {
                 justify-content: center;
                 flex-flow: column;
                 width: 50vh;
-                border-radius: 10px;
-                overflow: hidden;
-                box-shadow: 5px 10px 30px rgba(0, 0, 0, 0.085);
             }
 
             section header {
@@ -51,11 +66,13 @@ class MyElement extends LitElement {
             section select {
                 margin: 1rem;
                 width: 80%;
+                color: var(--text_lvl1);
                 text-align: center;
                 height: 40px;
                 border: none;
                 font-size: 14px;
                 border-bottom: 1px solid grey;
+                background-color: var(--bg_lvl2);
             }
 
             section figure {
@@ -63,7 +80,7 @@ class MyElement extends LitElement {
                 width: 100%;
                 margin: 0;
                 position: relative;
-                background-image: url(https://images.livemint.com/img/2021/06/03/600x338/_ABH0064-kaqB--621x414@LiveMint_1622730986371.JPG);
+                background-image: url(${unsafeCSS(this.cover)});
                 background-repeat: no-repeat;   
                 background-position: center;
                 background-size: cover;
@@ -87,12 +104,14 @@ class MyElement extends LitElement {
 
             section article {
                 padding-bottom: 0;
+                color: var(--text_lvl1);
             }
 
             section article > span {
                 margin: 1rem 0 0 0;
                 text-align: center;
                 width: 100%;
+                color: var(--text_lvl2);
                 display: inline-block;
             }
 
@@ -126,28 +145,49 @@ class MyElement extends LitElement {
         `;
     }
 
+    // Fetchs data from the news api
     fetchData() {
-        console.log(this.fetchURL)
+        // updating url based on user's choosen category
+        this.title = 'Fetching Data...';
+        this.content = 'Loading...';
+        this.author = '';
+        this.publisher = '';
+        this.url = '';
+        this.cover = '';
         this.fetchURL = `https://newsapi.org/v2/top-headlines?country=in&pageSize=50&category=${this.category}&apiKey=e61aa301fa3a4f76bd6c4387776a575e`;
         fetch(this.fetchURL)
         .then(response => response.json())
         .then(response => {
-            this.data = response;
-            console.log(this.data);
-            this.setData();}
+            // storing over 50 articles inside a variable to reduce api hits
+            this.data = response;   
+            this.setData();
+        // for updating data inside the component
+        }
         );
     }
 
+    // Updates data if user isn't intrested in the article
     setData() {
+        // cycles through fetched articles inside an array
         this.counter++;
-        this.title = this.data.articles[this.counter].title;
-        this.content = this.data.articles[this.counter].content;
-        this.author = this.data.articles[this.counter].author ? this.data.articles[this.counter].author : '-';
-        this.publisher = this.data.articles[this.counter].source.name;
-        this.url = this.data.articles[this.counter].url;
-        this.cover = this.data.articles[this.counter].urlToImage;
-    }
+        let artcInfo = this.data.articles[this.counter];
 
+        // checking to see if enough data is present
+        if (!artcInfo.urlToImage || (!artcInfo.content || !artcInfo.description)){
+            // if not the  move to next article
+            this.setData();
+            return;
+        }
+        // if data is there then update the component
+        this.title = artcInfo.title;
+        this.content = artcInfo.content ? artcInfo.content : artcInfo.description;
+        this.author = artcInfo.author ? artcInfo.author : '-';
+        this.publisher = artcInfo.source.name;
+        this.url = artcInfo.url;
+        this.cover = artcInfo.urlToImage;
+    }
+    
+    // Updates category fo articles once the user chooses another
     updateChoice() {
         this.category = this.shadowRoot.querySelector('select').value;
         this.counter = -1;
@@ -159,7 +199,7 @@ class MyElement extends LitElement {
         <section>
         <header>CONCAVE</header>
             <select @change="${this.updateChoice}" name="" id="">
-                <option value="business">Business</option>
+                <option value="business" selected>Business</option>
                 <option value="entertainment">Entertainment</option>
                 <option value="health">Health</option>
                 <option value="science">Science</option>
